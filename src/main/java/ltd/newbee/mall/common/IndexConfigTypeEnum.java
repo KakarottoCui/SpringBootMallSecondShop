1e1d1c3354b78ecf55c15167fb413808e91fbd59
cuifengbo <471363336@qq.com>
2023-08-03 09:58:32 +0800

    1

---
+++ src/main/java/ltd/newbee/mall/common/IndexConfigTypeEnum.java
@@ -0,0 +1,46 @@
+//微信：egvh56ufy7hh ，QQ：821898835
+package ltd.newbee.mall.common;
+
+
+
+    DEFAULT(0, "DEFAULT"),
+    INDEX_SEARCH_HOTS(1, "INDEX_SEARCH_HOTS"),
+    INDEX_SEARCH_DOWN_HOTS(2, "INDEX_SEARCH_DOWN_HOTS"),
+    INDEX_GOODS_HOT(3, "INDEX_GOODS_HOTS"),
+    INDEX_GOODS_NEW(4, "INDEX_GOODS_NEW"),
+    INDEX_GOODS_RECOMMOND(5, "INDEX_GOODS_RECOMMOND");
+
+    private int type;
+
+    private String name;
+
+    IndexConfigTypeEnum(int type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+
+    public static IndexConfigTypeEnum getIndexConfigTypeEnumByType(int type) {
+        for (IndexConfigTypeEnum indexConfigTypeEnum : IndexConfigTypeEnum.values()) {
+            if (indexConfigTypeEnum.getType() == type) {
+                return indexConfigTypeEnum;
+            }
+        }
+        return DEFAULT;
+    }
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/ltd/newbee/mall/dao/AdminUserMapper.java
@@ -0,0 +1,6 @@
+//微信：egvh56ufy7hh ，QQ：821898835
+package ltd.newbee.mall.dao;
+
+import ltd.newbee.mall.entity.AdminUser;
+import org.apache.ibatis.annotations.Param;
+
